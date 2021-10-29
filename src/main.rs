000316e8213e3e51daa8704c28f15dbc969316d4
boraarslan <bora-arslan@outlook.com>
2021-10-30 02:16:48 +0300

    Added delete scheduler

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,26 @@ mod entity;
 
 use axum::{
     extract::{
-        self,
-        multipart::MultipartRejection,
-        rejection::{self, ContentLengthLimitRejection},
-        ContentLengthLimit, Multipart, Path,
+        self, multipart::MultipartRejection, rejection::ContentLengthLimitRejection,
+        ContentLengthLimit, Multipart,
     },
     handler::{get, post},
     http::StatusCode,
     routing::BoxRoute,
     service, AddExtensionLayer, Router,
 };
-use chrono::{self, Utc};
+use chrono::{self, Duration, Utc};
 use dotenv::dotenv;
 use entity::*;
 use mime;
-use sea_orm::{ActiveModelTrait, Database, DatabaseConnection, EntityTrait, Set};
+use sea_orm::{
+    prelude::*, ActiveModelTrait, Database, DatabaseConnection, EntityTrait, QueryFilter, Set,
+};
 use std::{convert::Infallible, env};
+use tokio::{spawn, time::interval};
 use tower_http::{services::ServeDir, trace::TraceLayer};
 use tree_magic_mini as check_mime_content;
 use uuid::Uuid;
-
-// TODO! Write an async deleter function that will run with the server
-// and make db call for every 10 seconds for the photos that will be
-// deleted.
 
 #[tokio::main]
 async fn main() {
@@ -38,6 +35,9 @@ async fn main() {
     let db = Database::connect(env::var("DATABASE_URL").unwrap())
         .await
         .unwrap();
+    
+    spawn(delete_scheduler(db.clone()));
+
     let app = app(db);
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -45,25 +45,42 @@ async fn main() {
         .unwrap();
 }
 
-async fn root_get_handler() -> String {
-    "Hello World!".to_string()
-}
+async fn delete_scheduler(db: DatabaseConnection) {
+    let mut interval = interval(tokio::time::Duration::from_secs(10));
+    loop {
+        interval.tick().await;
+        let deadline = Utc::now().naive_utc() - Duration::seconds(10);
 
-async fn greet_user(user_id: Result<Path<u32>, rejection::PathParamsRejection>) -> String {
-    match user_id {
-        Ok(user_id) => {
-            format!("Hello User {}!", user_id.0)
-        }
-        Err(rejection::PathParamsRejection::InvalidPathParam(_)) => {
-            format!("Please provide a number within valid range.")
-        }
-        Err(rejection::PathParamsRejection::MissingRouteParams(_)) => {
-            format!("Missing user id.")
-        }
-        Err(_) => {
-            unreachable!()
+        let timegated_photos: Vec<photo_data::Model> = photo_data::Entity::find()
+            .filter(photo_data::Column::Timestamp.lt(deadline))
+            .all(&db)
+            .await
+            .unwrap_or_default();
+        println!("{:?}", timegated_photos);
+
+        for to_delete in timegated_photos {
+            let to_delete: photo_data::ActiveModel = to_delete.into();
+            let photo_data = to_delete.clone();
+            // even if it fails now it will probably will get deleted next iter.
+            if let Err(_) = to_delete.delete(&db).await {
+                continue;
+            }
+
+            if let Err(_) = tokio::fs::remove_file(format!(
+                "user_shots/{}.jpeg",
+                photo_data.photo_id.clone().unwrap()
+            ))
+            .await
+            {
+                // if error occurs reinsert the entry
+                let _ = photo_data.insert(&db).await;
+            }
         }
     }
+}
+
+async fn root_get_handler() -> String {
+    "Hello World!".to_string()
 }
 
 async fn upload(
@@ -121,10 +138,14 @@ async fn upload(
                 if let Err(_) =
                     tokio::fs::write(format!("user_shots/{}.jpeg", file_name), data).await
                 {
-                    // This shouldn't fail. At least thats what i thought when i wrote this. 
-                    let premature_insert = photo_data::Entity::find_by_id(photo_uuid).one(&db).await.unwrap();
-                    // This shouldn't fail either since we (succesfully) inserted it just a moment ago. 
-                    let premature_insert: photo_data::ActiveModel = premature_insert.unwrap().into();
+                    // This shouldn't fail. At least thats what i thought when i wrote this.
+                    let premature_insert = photo_data::Entity::find_by_id(photo_uuid)
+                        .one(&db)
+                        .await
+                        .unwrap();
+                    // This shouldn't fail either since we (succesfully) inserted it just a moment ago.
+                    let premature_insert: photo_data::ActiveModel =
+                        premature_insert.unwrap().into();
                     // TODO! If this operation fails init the standart delete sequence. or smthng like that.
                     let _delete_result = premature_insert.delete(&db).await;
                     return (
@@ -153,7 +174,6 @@ async fn upload(
 fn app(db_connection: DatabaseConnection) -> Router<BoxRoute> {
     Router::new()
         .route("/upload", post(upload))
-        .route("/user/:id", get(greet_user))
         .nest(
             "/img",
             service::get(ServeDir::new("user_shots")).handle_error(|error: std::io::Error| {
