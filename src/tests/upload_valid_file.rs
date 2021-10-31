5a9952f9aa15f92cdef8eca603e57e6476c56922
boraarslan <bora-arslan@outlook.com>
2021-10-31 20:11:05 +0300

    Added default env_var values

--- src/tests/upload_valid_file.rs
+++ src/tests/upload_valid_file.rs
@@ -3,7 +3,8 @@ use helpers::build_multipart_request;
 #[tokio::test]
 async fn upload_valid_file() {
     dotenv().ok();
-    let db = Database::connect(env::var("DATABASE_URL").unwrap()).await.unwrap();
+    let database_url = env::var("DATABASE_URL").unwrap_or("postgres://localhost:5432/postgres".to_string());
+    let db = Database::connect(database_url).await.unwrap();
     let app = app(db);
     let listener = TcpListener::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).unwrap();
     let addr = listener.local_addr().unwrap();
